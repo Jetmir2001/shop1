5e5cf1e93f233c3dfa90e8c24555f7b7f40caf0a
Jetmir2001 <153080159+Jetmir2001@users.noreply.github.com>
2024-03-09 12:29:28 +0100

    Add files via upload

---
+++ resta.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal ({
+    origin: 'top',
+    distance: '40px',
+    duration: 1000,
+    reset: true
+
+});
+
+sr.reveal('.home-text,.home-img,.about-img, .about-text, .box, .s-box, .connect-text, .btn , .contact-box') 
